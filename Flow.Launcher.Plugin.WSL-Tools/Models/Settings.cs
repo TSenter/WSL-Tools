9a2c3c6cf3dc20832fd70768dfbd043492e0f3a7
TSenter <tyler.senter.427@gmail.com>
2024-11-03 21:57:09 -0500

    feat: Add command to open directory or workspace in VS Code

---
+++ Flow.Launcher.Plugin.WSL-Tools/Models/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace Flow.Launcher.Plugin.WSLTools.Core
+{
+  public class Settings
+  {
+    public string gitFolder { get; set; } = "/git";
+    public string distroName { get; set; } = "Ubuntu";
+    public bool useSSHGit { get; set; } = false;
+  }
+}
---
+++ Flow.Launcher.Plugin.WSL-Tools/UI/SettingsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Flow.Launcher.Plugin.WSLTools.Core;
+
+namespace Flow.Launcher.Plugin.WSLTools.UI
+{
+  public class SettingsViewModel : BaseModel
+  {
+    public readonly PluginInitContext Context;
+    public Settings Settings { get; init; }
+
+    public SettingsViewModel(PluginInitContext context, Settings settings)
+    {
+      Settings = settings;
+      Context = context;
+    }
+
+    public bool useSSHGit
+    {
+      get => Settings.useSSHGit;
+      set
+      {
+        Settings.useSSHGit = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public string gitFolder
+    {
+      get => Settings.gitFolder;
+      set
+      {
+        Settings.gitFolder = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public string distroName
+    {
+      get => Settings.distroName;
+      set
+      {
+        Settings.distroName = value;
+        OnPropertyChanged();
+      }
+    }
+  }
+}
